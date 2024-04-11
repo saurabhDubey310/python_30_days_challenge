facf76f422ce8545aeec42347d3eed6639fecfa2
saurabh dubey <94182191+saurabhDubey310@users.noreply.github.com>
2024-04-11 23:01:52 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,74 @@
+# 'Day 2: 30 Days of python programming'
+# Exercise level:1
+
+# first_name = 'saurabh'
+# last_name = 'dubey'
+# full_name = 'saurabh dubey'
+# country = 'india'
+# city = 'valsad'
+# age = 22
+# year = 2002 
+# is_married = True
+# is_true = True
+# is_light_on = False
+# fathername , mothername  = 'surendra' ,'poonam'
+
+# print(first_name,last_name,fathername,mothername,is_married)
+
+
+# Exercise level 2
+
+# print(type(first_name))
+# print(type(last_name))
+# print(type(full_name))
+# print(type(country))
+# print(type(city))
+# print(type(is_light_on))
+# print(type(is_true))
+# print(len(first_name))
+
+# print(len(first_name))
+# print(len(last_name))
+
+
+# num_one = 5
+# num_two = 4
+
+# total = num_one+num_two
+# diff = num_one-num_two
+# product = num_one*num_two
+# divison =  num_one/num_two
+# remaider = num_one%num_two
+# exp = num_one**num_two
+# floor_division = num_one//num_two
+
+# print(total)
+# print(diff)
+# print(product)
+# print(divison)
+# print(remaider)
+# print(exp)
+# print(floor_division)
+
+# The radius of a circle is 30 meters.
+# radius = 30
+# area_of_circle = 3.14*(radius*radius)
+# print("area_of_circle: ",area_of_circle)
+
+
+# circum_of_circle = 2*3.14*(radius)
+# print("cirumference :",circum_of_circle)
+
+
+# radius = int(input("Enter radius : "))
+# print("area of circle :", 3.14*(radius*radius))
+
+first_name = input("Enter first_name: ")
+last_name = input("Enter last_name :") 
+country = input("Enter country :")
+age = int(input("Enter age :"))
+
+print("first_name :",first_name)
+print("last_name :",last_name)
+print("country :",country)
+print("age :",age)
