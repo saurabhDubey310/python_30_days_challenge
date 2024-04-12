065225db19f14fd72900682dcfd385f0491c009e
saurabh dubey <94182191+saurabhDubey310@users.noreply.github.com>
2024-04-12 22:34:07 +0530

    Update operators.py

--- operators.py
+++ operators.py
@@ -1,12 +1,12 @@
 #Exercise Day3:
 
-# age = 22
-# height = 170.22
-# complex = (1 + 2j)
+age = 22
+height = 170.22
+complex = (1 + 2j)
 
-# base = int(input("Enter base of triangle :"))
-# height = int(input("Enter height of triangle :"))
-# print("Area of the triangle is :",((0.5* base * height)))
+base = int(input("Enter base of triangle :"))
+height = int(input("Enter height of triangle :"))
+print("Area of the triangle is :",((0.5* base * height)))
 
 side_a = int(input("Enter side a of triangle :"))
 side_b = int(input("Enter side b of triangle :"))
