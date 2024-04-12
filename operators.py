a60d462c9c245f05e892868f8c4834be4585e943
saurabh dubey <94182191+saurabhDubey310@users.noreply.github.com>
2024-04-12 22:32:41 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,14 @@
+#Exercise Day3:
+
+# age = 22
+# height = 170.22
+# complex = (1 + 2j)
+
+# base = int(input("Enter base of triangle :"))
+# height = int(input("Enter height of triangle :"))
+# print("Area of the triangle is :",((0.5* base * height)))
+
+side_a = int(input("Enter side a of triangle :"))
+side_b = int(input("Enter side b of triangle :"))
+side_c = int(input("Enter side c of triangle :"))
+print("perimeter of triangle: " ,side_a + side_b + side_c)
